00380302485d18d58d4c014400a6ac37beadd0e1
jdabassett <jacobbassett@gmail.com>
2023-04-15 16:32:49 -0600

    minimum viable product

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <>
-        <p>Footer Boogers</p>
+        <p>All Powerful Creator: Jacob Bassett</p>
       </>
     )
   }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import HornedBeast from './HornedBeast.js'
+import data from '../data.json'
+
+// console.log(data)
 
 class Main extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      beastArray:data
+    }
+  }
+
+
   render() {
+    // console.log(this.state.beastArray)
+    //iterate over data to make horned beast elements
+    const HornedBeasts = this.state.beastArray.map(
+      (item,index)=>{
+        return <HornedBeast 
+          key={item._id}
+          title={item.title} 
+          imageUrl={item.image_url}
+          description={item.description}
+          horns={item.horns}
+          />})
+
     return (
       <>
-        <p>Main Boogers</p>
+        {HornedBeasts}
       </>
     )
   }
