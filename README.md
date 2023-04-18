11da397a9e9a3ef7089b4e06a218574f620da19f
jdabassett <jacobbassett@gmail.com>
2023-04-17 21:10:24 -0600

    finished with hearts icons

--- README.md
+++ README.md
@@ -9,3 +9,38 @@ Date: 4-15-2023
 Creator: Jacob Bassett
 Collaborators: 
 
+
+## Build Diary
+
+Feature#1:Display Images
+  *Estimated Time: 00:30
+  *Start Time: 19:00
+  *Finish Time: 19:40
+  *Actual Time: 00:40
+
+Feature#2: Allow Users to Favorite Individual Beasts
+  *Estimated Time: 01:00
+  *Start Time: 19:50
+  *Finish Time: 21:10
+  *Actual Time: 01:20
+
+Feature#3: Bootstrap
+  *Estimated Time:
+  *Start Time:
+  *Finish Time:
+  *Actual Time:
+
+Feature#4: Add Interaction
+  *Estimated Time:
+  *Start Time:
+  *Finish Time:
+  *Actual Time:
+
+
+
+
+Feature#: 
+  *Estimated Time:
+  *Start Time:
+  *Finish Time:
+  *Actual Time:
--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -1,21 +1,62 @@
-import React from 'react'
+import React from 'react';
+import {TbHeartFilled, TbHeartPlus, TbHeartMinus} from 'react-icons/tb';
 
 
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      hearts:0,
+      hoverState:false
+    }
+  }
+  
+  //change number of hearts per beast
+  handlerHearts = (num) => {
+    this.setState(prevState => ({...prevState,hearts:prevState.hearts+=num}))
+  }
 
+  //track hover state over beast
+  handlerHover = (bool) => {
+    this.setState(prevState => ({...prevState,hoverState:bool}))
+  }
 
   render() {
     // console.log(this.props)
+    // console.log(this.state.hoverState)
     return (
-      <>
+      <div 
+        onMouseEnter={()=>this.handlerHover(true)}
+        onMouseLeave={()=>this.handlerHover(false)}
+        >
         <h2>{this.props.title}</h2>
         <img 
           width='300px' 
           src={this.props.imageUrl} 
           alt={this.props.description} 
-          title={this.props.title} />
+          title={this.props.title} 
+          onClick={()=>this.handlerHearts(1)}
+          />
+        
+        {/* plus heart icon will only appear when hearts equal zero*/}
+        {this.state.hearts===0?
+          <TbHeartPlus onClick={()=>this.handlerHearts(1)}/>:
+          null}
+        {/* solid heart icon will only appear when hearts greater than zero*/}
+        {this.state.hearts>=1?
+          [<p>{this.state.hearts}</p>,<TbHeartFilled onClick={()=>this.handlerHearts(1)}/>]:
+          null}
+        {/* minus heart icon will only appear when hover state is true */}
+        {this.state.hearts>=1 && this.state.hoverState?
+          <TbHeartMinus onClick={()=>this.handlerHearts(-1)}/>:
+          null}
+
+
+        
+
+
         <p>{this.props.description}</p>
-      </>
+      </div>
     )
   }
 }
